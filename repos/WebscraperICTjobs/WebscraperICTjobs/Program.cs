758e68e1ba27f770acf033672bbadd77a5f2e3fc
JonasBaelus <r0901372@student.thomasmore.be>
2022-12-18 21:51:58 +0100

    initial commit

---
+++ repos/WebscraperICTjobs/WebscraperICTjobs/Program.cs
@@ -0,0 +1,53 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Text;
+
+
+public class Vacature
+{
+    public string Titel { get; set; }
+    public string Bedrijf { get; set; }
+    public string Locatie { get; set; }
+    public string Keywords { get; set; }
+    public string Link { get; set; }
+}
+
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+
+        StringBuilder csv = new StringBuilder();
+        string seperator = ",";
+        string[] headings = { "link", "titel", "bedrijf", "locatie", "keywords" };
+        csv.AppendLine(string.Join(seperator, headings));
+        Console.WriteLine("Welke zoekterm wilt u opzoeken?");
+        string zoekterm = Console.ReadLine();
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl("https://www.ictjob.be/nl/it-vacatures-zoeken?keywords="+zoekterm);
+        Thread.Sleep(5000);
+        driver.FindElement(By.XPath("//*[@id=\"body-ictjob\"]/div[2]/a")).Click();
+        driver.FindElement(By.Id("sort-by-date")).Click();
+
+        Thread.Sleep(10000);
+
+        var link = driver.FindElements(By.ClassName("search-item-link"));
+        var titel = driver.FindElements(By.ClassName("job-title"));
+        var bedrijf = driver.FindElements(By.ClassName("job-company"));
+        var locatie = driver.FindElements(By.ClassName("job-location"));
+        var keywords = driver.FindElements(By.ClassName("job-keywords"));
+
+        List<Vacature> vacatureList = new List<Vacature> { new Vacature { Link = link.ElementAt(0).GetAttribute("href"), Titel = titel.ElementAt(0).Text, Bedrijf = bedrijf.ElementAt(0).Text, Locatie = locatie.ElementAt(0).Text, Keywords = keywords.ElementAt(0).Text } };
+        for (var i = 0; i < 5; i++)
+        {
+            string[] tekst = { link.ElementAt(i).GetAttribute("href").ToString(), titel.ElementAt(i).Text, bedrijf.ElementAt(i).Text, locatie.ElementAt(i).Text, keywords.ElementAt(i).Text };
+            csv.AppendLine(string.Join(seperator, tekst));
+
+        }
+
+        File.WriteAllText(@"C:\Users\jbael\source\repos\WebscraperICTjobs\WebscraperICTjobs\file.csv", csv.ToString());
+        driver.Close();
+
+    }
+}
---
+++ repos/WebscraperVoorbeeld/WebscraperVoorbeeld/Program.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Text;
+
+
+public class Video
+{
+    public string Link { get; set; }
+    public string Titel { get; set; }
+    public string Weergaves { get; set; }
+    public string Uploader { get; set; }
+}
+
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+
+        StringBuilder csv = new StringBuilder();
+        string seperator = ",";
+        string[] headings = { "link", "titel", "weergaves", "uploader" };
+        csv.AppendLine(string.Join(seperator, headings));
+        Console.WriteLine("Welke zoekterm wilt u opzoeken?");
+        string zoekterm = Console.ReadLine();
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl("https://www.youtube.com/results?search_query="+zoekterm+"&sp=CAI%253D");
+        Thread.Sleep(5000);
+        driver.FindElement(By.XPath("//*[@id=\"content\"]/div[2]/div[6]/div[1]/ytd-button-renderer[2]/yt-button-shape/button/yt-touch-feedback-shape/div/div[2]")).Click();
+        Thread.Sleep(10000);
+
+        var link = driver.FindElements(By.XPath("//*[@id=\"video-title\"]"));
+        var titel = driver.FindElements(By.XPath(".//*[@id=\"video-title\"]/yt-formatted-string"));
+        var weergaves = driver.FindElements(By.XPath("//*[@id=\"metadata-line\"]/span[1]"));
+        var uploader = driver.FindElements(By.XPath("//*[@id=\"text\"]/a"));
+
+        List<Video> videoList = new List<Video> { new Video { Link = link.ElementAt(0).GetAttribute("href"), Titel = titel.ElementAt(0).Text, Weergaves = weergaves.ElementAt(0).Text, Uploader = uploader.ElementAt(0).Text } };
+        for (var i = 0; i < 5; i++)
+        {
+            string[] tekst = { link.ElementAt(i).GetAttribute("href"), titel.ElementAt(i).Text, weergaves.ElementAt(i).Text, uploader.ElementAt(i).Text };
+            csv.AppendLine(string.Join(seperator, tekst));
+            
+        }
+        
+        File.WriteAllText(@"C:\Users\jbael\source\repos\WebscraperVoorbeeld\WebscraperVoorbeeld\file.csv", csv.ToString());
+        driver.Close();
+
+    }
+}
